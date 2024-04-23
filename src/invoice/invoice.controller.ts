4df3e219ebe29d6361a6048b65ebfa8c953760f3
Cristi Pele <cristian.pele@adonissoft.com>
2024-04-23 18:07:43 +0300

    recursive brackets test

--- src/invoice/invoice.controller.ts
+++ src/invoice/invoice.controller.ts
@@ -1,10 +1,65 @@
-import { BadRequestException, Controller, Get, Param } from '@nestjs/common';
+import {
+  BadRequestException,
+  Body,
+  Controller,
+  Get,
+  Param,
+  Post,
+} from '@nestjs/common';
 import { InvoiceService } from './invoice.service';
 import { PrismaUser } from 'src/auth/auth.decorators';
 
 @Controller('invoice')
 export class InvoiceController {
   constructor(private readonly invoiceService: InvoiceService) {}
+
+  recursiveBrackets = (brackets: string): number => {
+    if (brackets === '()') {
+      return 0;
+    }
+
+    const splitBrackets = brackets.split('()');
+    if (splitBrackets.length > 1) {
+      return this.recursiveBrackets(splitBrackets.join(''));
+    } else {
+      const arrayBrackets = Array.from(brackets);
+      const newNrSwape: number[] = [Number.MAX_SAFE_INTEGER];
+      for (let i = 0; i < brackets.length; i++) {
+        for (let j = i + 1; j < brackets.length; j++) {
+          if (arrayBrackets[i] !== arrayBrackets[j]) {
+            const newArrayBrackets = [...arrayBrackets];
+            const ib = newArrayBrackets[i];
+            newArrayBrackets[i] = newArrayBrackets[j];
+            newArrayBrackets[j] = ib;
+            newNrSwape.push(
+              this.recursiveBrackets(newArrayBrackets.join('')) + 1,
+            );
+          }
+        }
+      }
+      return Math.min(...newNrSwape);
+    }
+  };
+
+  @Post('/')
+  async test(@Body() body: { brackets: string }) {
+    const brackets = body.brackets;
+
+    let nrStart = 0;
+    let nrEnd = 0;
+    for (let i = 0; i < brackets.length; i++) {
+      if (brackets[i] === '(') {
+        nrStart++;
+      } else {
+        nrEnd++;
+      }
+    }
+    if (nrStart !== nrEnd) {
+      return -1;
+    }
+
+    return this.recursiveBrackets(brackets);
+  }
 
   @Get('/')
   async getInvoices(@PrismaUser() userID: number) {
